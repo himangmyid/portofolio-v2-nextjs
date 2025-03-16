96935045758200e6dcb92a2b343e5693b56cef0b
Benidiktus Himang <46196768+himangmyid@users.noreply.github.com>
2025-03-16 14:00:04 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,35 @@ const oswald = Oswald({ weight: ["400", "700"], subsets: ["latin"], variable: "-
 const quicksand = Quicksand({ weight: ["400", "700"], subsets: ["latin"], variable: "--font-quicksand" });
 const sofiaSans = Sofia_Sans({ weight: ["400", "700"], subsets: ["latin"], variable: "--font-sofia-sans" });
 
+export const metadata = {
+    title: "Portfolio v2 | HIMANG",
+    description: "Explore the portfolio of HIMANG, a web developer specializing in Next.js, Tailwind CSS, and UI/UX design.",
+    keywords: ["portfolio", "web developer", "full-stack", "Next.js", "Tailwind CSS", "UI/UX"],
+    authors: [{ name: "HIMANG", url: "https://www.himang.my.id/" }],
+    openGraph: {
+      title: "Portfolio v2 | HIMANG",
+      description: "Explore the portfolio of HIMANG, a web developer specializing in Next.js, Tailwind CSS, and UI/UX design.",
+      url: "https://portofolio-v2-dun.vercel.app/",
+      siteName: "HIMANG Portfolio",
+      images: [
+        {
+          url: "/me.png", // Ganti dengan gambar profil atau thumbnail
+          width: 800,
+          height: 800,
+          alt: "HIMANG Profile Picture",
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary",
+      title: "Portfolio v2 | HIMANG",
+      description: "Explore the portfolio of HIMANG, a web developer specializing in Next.js, Tailwind CSS, and UI/UX design.",
+      images: ["/me.png"], // Gambar untuk preview di Twitter
+    },
+  };
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" data-theme="dark" className={`${kanit.variable} ${oswald.variable} ${quicksand.variable} ${sofiaSans.variable}`}>
