0921fe27affc80385057d8816c53cecb15514c5b
Himangmyid <himangbd181021@gmail.com>
2025-03-23 15:24:47 +0800

    Update  bug avatar github nextcnfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
-        domains: ["api.dicebear.com"
+        domains: ["avatars.githubusercontent.com", "api.dicebear.com",
         ],
         dangerouslyAllowSVG: true,
     },
